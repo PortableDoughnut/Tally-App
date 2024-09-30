a58da0d9272d6acaf6dcd53832a141dd4e8644e5
PortableDoughnut <gwen.thelin@gmail.com>
2024-09-30 08:19:44 -0600

    Initial Commit

---
+++ Tally App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tally App
+//
+//  Created by Gwen Thelin on 9/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Tally AppTests/Tally_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tally_AppTests.swift
+//  Tally AppTests
+//
+//  Created by Gwen Thelin on 9/30/24.
+//
+
+import Testing
+@testable import Tally_App
+
+struct Tally_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
